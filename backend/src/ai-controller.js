9d147b276f18b921adc83917db0d049c850040fb
bmltera <billmli88@gmail.com>
2024-08-08 22:36:14 -0700

    connect to openai

--- backend/src/ai-controller.js
+++ backend/src/ai-controller.js
@@ -1,3 +1,6 @@
+import testcall from './aitasks/test-api.js';
+import gpt3test from './aitasks/gpt3-test.js';
+
 const responseSchema = {
     response: {
         200: {
@@ -9,11 +12,42 @@ const responseSchema = {
     }
 }
 
+const responseSchema2 = {
+    response: {
+        200: {
+            properties: {
+                message: {type: 'object'}
+            },
+            required: ['message']
+        }
+    }
+}
+
 
 const aiController = (fastify, options, done) => {
     fastify.get('/',{schema: responseSchema},(req,reply) => {
         return {
             message: 'openAI api'
+        };
+    });
+
+    // test call to openAI api
+    fastify.get('/testcall', async function() {
+
+        const result = await testcall();
+        
+        return {
+            result
+        };
+    });
+
+    // test call to gpt assistant
+    fastify.get('/gpt3test', async function() {
+
+        const result = await gpt3test();
+        
+        return {
+            result
         };
     });
 
