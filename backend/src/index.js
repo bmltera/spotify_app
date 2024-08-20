6a03e757272d92e11eca2c3305be564289e828ef
bmltera <billmli88@gmail.com>
2024-08-20 00:04:12 -0700

    cors origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,34 @@ import Fastify from 'fastify';
 import greetingsController from './greetings-controller.js';
 import aiController from './ai-controller.js';
 import cors from '@fastify/cors'
+import fs from 'fs';
+// import { fileURLToPath } from 'url';
+// import path from 'path';
+
 import spotifyController from './spotify-controller.js';
 
 
+// const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+// const __dirname = path.dirname(__filename); // get the name of the directory
+
+// const keyPath = path.resolve(__dirname, './secure/key.pem');
+// const certPath = path.resolve(__dirname, './secure/cert.pem');
+
+// // Load SSL/TLS certificates
+// const httpsOptions = {
+//     key: fs.readFileSync(keyPath),
+//     cert: fs.readFileSync(certPath)
+//   };
+
 const fastify = Fastify({
-    logger: true
+    logger: true,
+    //ttps: httpsOptions
 });
 
 // cors controller
 await fastify.register(cors, { 
     origin: 'http://localhost:5173', // allow requests from this origin
-    origin: '98.51.84.115',
+    origin: 'http://98.51.84.115',
     methods: ['GET', 'POST'] // specify allowed methods
   })
 
